7fbe019a0c0862df91abfe91770422a406d20308
github username <github email address>
2020-03-10 09:42:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+// document.body.onload = addElement
+
+let element = document.querySelector('main');
+element.remove()
+
+// function addElement(){
+//     let newHeader = document.createElement('h1');
+//     newHeader.setAttribute("victory", "h1" )
+//     let newText = document.createTextNode("YOUR NAME is the champion");
+//     newHeader.appendChild(newText)
+
+//     let currentHead = document.getElementById("victory");
+//     document.body.insertBefore(newHeader, currentHead);
+// }
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "David is the champion"
