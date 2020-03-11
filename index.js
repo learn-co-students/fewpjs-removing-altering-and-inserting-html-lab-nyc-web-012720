e314f99a8523a7eda66ced87aa43cfadb0a7f8b8
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-10 20:14:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Let's get it started!");
+console.log("Let's get it started!"); 
+
+let mainNode = document.querySelector("#main");
+
+mainNode.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory";
+newHeader.innerHTML = "April is the champion";
